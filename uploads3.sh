e369f55183761559aae7ce48c1383410f55022a3
Ofoedu Frank Ebuka <Frankofoedu@gmail.com>
2020-06-10 12:12:10 +0100

    Update uploads3.sh

--- uploads3.sh
+++ uploads3.sh
@@ -1,3 +1,6 @@
+#create s3 bucket
+aws s3 mb s3://pytorch-lambda
+
 # copy to s3 and update lambda function
-aws s3 cp pytorch_fn.zip s3://lambda-functions/
+aws s3 cp pytorch_fn.zip s3://pytorch-lambda/
 aws lambda update-function-code --function-name pytorch_example \   --s3-bucket lambda-functions --s3-key pytorch_fn.zip
